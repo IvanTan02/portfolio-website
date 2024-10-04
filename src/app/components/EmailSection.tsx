68e8995485679e190b96b53534da879006dccdc4
Ivan Tan <ivantanyenwen02@gmail.com>
2024-10-04 11:50:37 +0800

    minor bug fix

--- src/app/components/EmailSection.tsx
+++ src/app/components/EmailSection.tsx
@@ -39,7 +39,7 @@ const EmailSection = () => {
     <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative'>
       {/* <div className="absolute w-80 h-80 z-0 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full blur-lg top-52 -left-4 transform -translate-x-1/2 -translate-y-1/2"></div> */}
       <div>
-        <h5 className='text-xl font-bold text-white my-2'>Let's Connect</h5>
+        <h5 className='text-xl font-bold text-white my-2'>{`Let's Connect`}</h5>
         <p className='text-[#ADB7BE] mb-4 max-w-md'>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae quos dolore ipsum iusto ducimus blanditiis eum possimus. Vel sint doloremque animi! Nam, expedita officiis in necessitatibus fugit adipisci fuga autem.
         </p>

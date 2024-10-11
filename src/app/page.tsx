68e1b5699be40e2b207e44f818c2b651f17baca1
Ivan Tan <ivantanyenwen02@gmail.com>
2024-10-11 08:15:51 +0800

    update about me section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Footer from "@/components/common/footer/Footer";
 import Navbar from "@/components/common/navbar/Navbar";
 import AboutSection from "@/components/home/about/AboutSection";
-import AchievementSection from "@/components/home/achievements/AchievementSection";
 import EmailSection from "@/components/home/email/EmailSection";
 import HeroSection from "@/components/home/hero/HeroSection";
 import ProjectSection from "@/components/home/projects/ProjectSection";
@@ -11,9 +10,9 @@ export default function Home() {
     <main className="flex flex-col min-h-screen bg-base-300">
       <Navbar />
       <div className="container mx-auto">
-        <section id="hero" className="min-h-screen flex flex-col justify-center items-center"><HeroSection /></section>
-        <section id="about" className="mb-16" style={{ scrollMarginTop: '120px' }}><AboutSection /></section>
-        <section id="projects" style={{ scrollMarginTop: '150px' }}><ProjectSection /></section>
+        <section id="hero" className="flex flex-col justify-center items-center" style={{ marginTop: '15em', marginBottom: '10em' }}><HeroSection /></section>
+        <section id="about" className="mb-32"><AboutSection /></section>
+        <section id="projects"><ProjectSection /></section>
         <section id="contact"><EmailSection /></section>
       </div>
       <Footer />
--- src/components/home/about/AboutSection.tsx
+++ src/components/home/about/AboutSection.tsx
@@ -1,21 +1,46 @@
 'use client';
-import React, { useState, useTransition } from 'react'
-import TabButton from '../../common/TabButton';
-import { CONSTANTS, MY_SKILLS } from '@/environments/constant';
-import Image from "next/image"
+import { CONSTANTS } from '@/environments/constant';
 import styles from "./AboutSection.module.css";
+import { FaBolt, FaBook, FaGraduationCap, FaLaptopCode } from 'react-icons/fa6';
+
+const timelineItems = [
+  { timeframe: 'Aug 2021', icon: <FaBook />, description: `Enrolled in Taylor's University` },
+  { timeframe: 'Apr 2024', icon: <FaBolt />, description: `First experience in the industry at my internship` },
+  { timeframe: 'Aug 2024', icon: <FaGraduationCap />, description: `Graduated with a Bachelor's Degree in Computer Science` },
+  { timeframe: 'Present', icon: <FaLaptopCode />, description: `Started my journey as a software engineer` },
+]
 
 const AboutSection = () => {
   return (
-    <section className='text-white'>
-      <div className="flex flex-col justify-center items-center">
+    <section className='text-neutral-content w-full px-10 lg:px-24'>
+      <div className="flex flex-col justify-center items-center w-full">
         <h2 className={styles.about_me_title}>About Me</h2>
       </div>
-      <div className="w-full flex flex-row justify-center items-center">
-        <div className={`col-span-11 mx-16 pt-5 ${styles.about_me_text}`}>
+      <div className="flex flex-row justify-center items-center mt-8">
+        {/* Summary */}
+        <div className={`${styles.about_me_text}`}>
           {CONSTANTS.heroContent.summary}
         </div>
       </div>
+      {/* Timeline */}
+      <ul className={`timeline timeline-vertical lg:timeline-horizontal ${styles.timeline_container}`}>
+        {
+          timelineItems.map((timelineItem, index) => {
+            return (<li key={index} className={`${styles.timeline_item}`}>
+              {index > 0 && <hr className={`bg-neutral-content ${styles.timeline_line}`} />}
+              <div className={`timeline-start ${styles.timeline_timeframe}`}>{timelineItem.timeframe}</div>
+              <div className="timeline-middle">
+                <div className={`${styles.timeline_icon}`}>
+                  {timelineItem.icon}
+                </div>
+              </div>
+              <div className={`timeline-end timeline-box ${styles.timeline_description}`}>{timelineItem.description}</div>
+              {index < (timelineItems.length - 1) && <hr className={`bg-neutral-content ${styles.timeline_line}`} />}
+            </li>)
+          })
+        }
+      </ul>
+
     </section>
   )
 }
